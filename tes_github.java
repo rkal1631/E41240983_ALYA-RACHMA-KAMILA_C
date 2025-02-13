10c6ef07b5769324a0eb3537cd401a6fc67cd866
rkal1631 <alrk1631@gmail.com>
2025-02-13 11:23:57 +0700

    Create tes_github.java

---
+++ tes_github.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alya
+ */
+public class tes_github {
+  public static void main(String[] args) {
+        System.out.println("Alya Rachma Kamila_E41240983_C");
+    }
+  }
